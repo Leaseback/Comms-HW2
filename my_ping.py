08d0286f5471955b706cc1dcfe25699091cc0de0
Kyle Kaczorowski <69587926+Leaseback@users.noreply.github.com>
2025-02-21 15:34:52 -0500

    Added docstring to all functions

--- my_ping.py
+++ my_ping.py
@@ -8,6 +8,15 @@ import select
 
 
 def checksum(source_string):
+    """
+        Generates a checksum for a given string (used for packet validation).
+
+        Args:
+            source_string (bytes): The data for which the checksum is to be generated.
+
+        Returns:
+            int: The checksum value for the given data.
+        """
     count = len(source_string) % 2
     sum = 0
     for i in range(0, len(source_string) - count, 2):
@@ -21,6 +30,16 @@ def checksum(source_string):
 
 
 def create_packet(id, data_size):
+    """
+            Given packet_id and a desired packet size created a ICMP Echo packet
+
+            Args:
+                id (int): The packet ID / Number
+                data_size: The amount of bytes to pack into the packet
+
+            Returns:
+                Packet: A ICMP Echo Packet of ID id of size data_size
+            """
     header = struct.pack('!BBHHH', 8, 0, 0, id, 1)
     data = bytes([i % 256 for i in range(data_size)])  # More robust data generation
     checksum_val = checksum(header + data)
@@ -29,6 +48,17 @@ def create_packet(id, data_size):
 
 
 def receive_ping(sock, id, timeout):
+    """
+                Listens for pings on socket sock
+
+                Args:
+                    sock (Socket): The socket to listen on
+                    id (int): The packet ID
+                    timeout (int): Time in seconds to wait before timeing out
+
+                Returns:
+                    Packet: The received packet or None if timed out
+                """
     time_received = time.time()
     while time.time() - time_received < timeout:
         ready = select.select([sock], [], [], timeout - (time.time() - time_received))
@@ -43,6 +73,19 @@ def receive_ping(sock, id, timeout):
 
 
 def ping(host, count=4, interval=1, timeout=1, data_size=56):
+    """
+                Pings a given host address and prints the response (if any)
+
+                Args:
+                    host (Str): The ip address or hostname to ping
+                    count (int) (default 4): Amount of packets to send
+                    interval (int) (default 1): Time to wait between sending each packet
+                    timeout (int) (default 1): Time to wait for packet before timing out
+                    data_size (int) (default 56): Size of packets to send when pinging
+
+                Returns:
+                    None
+                """
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
     except PermissionError:
@@ -79,6 +122,9 @@ def ping(host, count=4, interval=1, timeout=1, data_size=56):
 
 
 def main():
+    """
+                Takes in user command and arguments, parsing them and calls the ping() function accordingly
+    """
     parser = argparse.ArgumentParser(description="Ping utility")
     parser.add_argument("-ping", nargs="?", type=str, help="Ping dest IP", required=True)
     parser.add_argument("-c", default=4, nargs="?", type=int, help="Number of packets to send")
--- my_traceroute.py
+++ my_traceroute.py
@@ -7,6 +7,15 @@ import select
 
 
 def checksum(source_string):
+    """
+            Generates a checksum for a given string (used for packet validation).
+
+            Args:
+                source_string (bytes): The data for which the checksum is to be generated.
+
+            Returns:
+                int: The checksum value for the given data.
+            """
     count = len(source_string) % 2
     sum = 0
     for i in range(0, len(source_string) - count, 2):
@@ -20,6 +29,16 @@ def checksum(source_string):
 
 
 def create_packet(id, data_size):
+    """
+                Given packet_id and a desired packet size created a ICMP Echo packet
+
+                Args:
+                    id (int): The packet ID / Number
+                    data_size: The amount of bytes to pack into the packet
+
+                Returns:
+                    Packet: A ICMP Echo Packet of ID id of size data_size
+                """
     header = struct.pack('!BBHHH', 8, 0, 0, id, 1)
     data = bytes([i % 256 for i in range(data_size)])  # More robust data generation
     checksum_val = checksum(header + data)
@@ -28,6 +47,18 @@ def create_packet(id, data_size):
 
 
 def receive_traceroute(sock, id, ttl, timeout):
+    """
+                    Listens for pings on socket sock
+
+                    Args:
+                        sock (Socket): The socket to listen on
+                        id (int): The packet ID
+                        timeout (int): Time in seconds to wait before timeing out
+                        ttl (int): Time to live [Unused, can be used to adjust starting ttl]
+
+                    Returns:
+                        Packet: The received packet or None if timed out
+                    """
     time_received = time.time()
     while time.time() - time_received < timeout:
         ready = select.select([sock], [], [], timeout - (time.time() - time_received))
@@ -51,6 +82,20 @@ def receive_traceroute(sock, id, ttl, timeout):
 
 
 def traceroute(host, print_num, print_summary, count=1):
+    """
+                    Pings a given host address, incrementing TTL each time and printing all intermediate responses
+                    that sent the packet back due to expired TTL
+
+                    Args:
+                    host (Str): The ip address or hostname to ping count (int) (default 4): Amount of packets
+                    to send
+
+                    print_num (boolean): If true, prints all addresses as numerical IPs print_summary (
+                    boolean): If true, prints a summary of how many probes were not answered for each hop.
+
+                    Returns:
+                        None
+                    """
     max_hops = 30
     timeout = 5
     data_size = 56
@@ -110,6 +155,9 @@ def traceroute(host, print_num, print_summary, count=1):
 
 
 def main():
+    """
+                    Takes in user command and arguments, parsing them and calls the traceroute() function accordingly
+        """
     parser = argparse.ArgumentParser(description="Traceroute utility")
     parser.add_argument("-traceroute", nargs="?", type=str, help="Trace dest IP", required=True)
     parser.add_argument("-n", action="store_true",
